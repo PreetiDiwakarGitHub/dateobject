88e877e1ed63a924b49ea49caf87ec819ec711dd
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-27 09:21:39 +0530

    first commit

---
+++ dateobject/object.js
@@ -0,0 +1,78 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+
+// give only date
+console.log(myDate.getDate());
+
+// the day of the week in Number(0-6);
+console.log(myDate.getDay());
+
+// return year 
+console.log(myDate.getFullYear());
+
+// return hour
+console.log(myDate.getHours());
+
+// return the milliseconds
+console.log(myDate.getMilliseconds());
+
+// return minutes
+console.log(myDate.getMinutes());
+
+// return month
+console.log(myDate.getMonth());
+
+// the seconds
+console.log(myDate.getSeconds());
+
+// Returns the number of milliseconds since midnight Jan 1, 1970.
+console.log(myDate.getTime());
+
+// Returns the time difference between UTC time and local time, in minutes.
+console.log(myDate.getTimezoneOffset());
+
+// Returns the day of the month, according to universal time (from 1-31).
+
+console.log(myDate.getUTCDate());
+
+// Returns the day of the week, according to universal time (from 0-6).
+console.log(myDate.getUTCDay())
+
+// Returns the year, according to universal time (four digits).
+console.log(myDate.getUTCFullYear())
+
+ // Returns the hour, according to universal time (from 0-23).
+console.log(myDate.getUTCHours())
+
+// Returns the milliseconds, according to universal time (from 0-999).
+console.log(myDate.getUTCMilliseconds());
+
+// Returns the minutes, according to universal time (from 0-59).
+console.log(myDate.getUTCMinutes());
+
+// Returns the month, according to universal time (from 0-11).
+console.log(myDate.getUTCMonth())
+
+// Returns the seconds, according to universal time (from 0-59).
+console.log(myDate.getUTCSeconds())
+
+//  Sets the day of the month of a date object.
+console.log(23);
+
+// Sets the year (four digits) of a date object.
+console.log(2024,8,10);
+
+// Sets the hour of a date object.
+console.log(1,60,3600,)
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/dateobject.git
+// git push -u origin main
