71136c805d40298ebd1a745c12b01a7f6c078fb3
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-28 09:36:00 +0530

    question

---
+++ recursion/recusion.js
@@ -0,0 +1,39 @@
+// question 1   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// function sum1(num) {
+//     if(num == 0) {
+//         return 0;
+//     }
+    
+//     return num + sum1(num - 1);
+// }
+
+// console.log(sum1(5));
+
+
+// question 2 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// function fact(num){
+//     if(num<1){
+//         return 1;
+//     }
+//     return num*fact(num-1);
+// }
+// console.log(fact(5));
+
+// question 3 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// question  4  \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+// let arr = [1, 2, 3];
+// function product(i = 0) {
+//     if (arr.length <= i) {
+//         return 1;
+//     }
+//     return arr[i] * product(i + 1);
+// }
+// console.log(product());
+
+
+
